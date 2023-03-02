db43104112c8e60a18eae3b4c29e59be9c79c56b
amir <khrabakoko@gmail.com>
2023-03-02 19:28:33 +0200

    ceaser is Done

---
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SecurityLibrary
+{
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+        public string Encrypt(string plainText, int key)
+        {
+            string encripted = "";
+            foreach (char x in plainText) {
+                int num = ((int)x-97+key)%26;
+                char encripted_letter =(char)(num+97);
+                encripted += encripted_letter;
+            }
+            Console.WriteLine(encripted);
+            return encripted;
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            cipherText = cipherText.ToLower();
+            string decripted = "";
+            foreach (char x in cipherText)
+            {
+                int num = (((int)x - 97) - key) % 26;
+                if (num < 0) num += 26;
+                char decripted_letter = (char)(num + 97);
+                decripted += decripted_letter;
+            }
+            Console.WriteLine(decripted);
+            return decripted;
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
