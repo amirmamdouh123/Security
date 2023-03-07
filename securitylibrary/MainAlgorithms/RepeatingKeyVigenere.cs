4626ce8bb66467789ea53337a48bfe02471ba9a1
Tunnaa <126968780+Tunnaa@users.noreply.github.com>
2023-03-07 16:39:22 +0200

    Update RepeatingKeyVigenere.cs

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +8,74 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+
+        char[] e_letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            //Expandable string 
+            StringBuilder ki = new StringBuilder();
+            cipherText = cipherText.ToLower();
+            int flag = 1;
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+               
+                ki.Append(e_letters[( (Array.IndexOf(e_letters, cipherText[i]) - Array.IndexOf(e_letters, plainText[i])) +26)% 26]);
+            }
+
+            String key = ki[0].ToString();
+           
+            
+            while (true)
+            {
+                if (Encrypt(plainText, key.ToString()).ToString().Equals(cipherText))
+                {
+
+                    break;
+                }
+                else
+                {
+                    key += ki[flag];
+                    flag++;
+                }
+            }
+
+            return key.ToString();
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+           
+            string ki = key.ToUpper();
+            int kl = ki.Length;
+            string ct = cipherText.ToUpper();
+            int cl = ct.Length;
+            string plaintxt = "";
+            for (int i = 0; i < cl ; i++)
+            {
+                if (cl == kl)
+                    break;
+                ki += ki[i];
+            }
+            
+           
+            for (int j = 0; j  < ki.Length && j < cl; j++)
+            {
+                int res = (ct[j] - ki[j] + 26) % 26;
+                res += 'A';
+                plaintxt += (char)(res);
+            }
+            return plaintxt;
         }
 
         public string Encrypt(string plainText, string key)
         {
-
             Console.WriteLine(plainText);
             int diff = plainText.Length - key.Length;
+
             for (int i = 0; i < diff; i += 1)
             {
                 key += key[i];
@@ -35,7 +88,6 @@ namespace SecurityLibrary
             }
             Console.WriteLine(encripted);
             return encripted;
-
         }
     }
 }
