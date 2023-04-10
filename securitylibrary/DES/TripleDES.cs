5cb8aac3ac647d0b9f475a564812531cf44e32ca
Tunnaa <126968780+Tunnaa@users.noreply.github.com>
2023-04-10 23:04:58 +0200

    Update TripleDES.cs

--- securitylibrary/DES/TripleDES.cs
+++ securitylibrary/DES/TripleDES.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +11,24 @@ namespace SecurityLibrary.DES
     /// </summary>
     public class TripleDES : ICryptographicTechnique<string, List<string>>
     {
+        DES des = new DES();
         public string Decrypt(string cipherText, List<string> key)
         {
-            throw new NotImplementedException();
+           
+            return des.Decrypt(des.Encrypt(des.Decrypt(cipherText, key[0]), key[1]), key[0]);
+
         }
 
         public string Encrypt(string plainText, List<string> key)
         {
-            throw new NotImplementedException();
+            
+            string x = des.Encrypt(plainText, key[0]);
+            string y = des.Decrypt(x, key[1]);
+            string cipher = des.Encrypt(y, key[0]);
+            return cipher;
         }
 
-        public List<string> Analyse(string plainText,string cipherText)
+        public List<string> Analyse(string plainText, string cipherText)
         {
             throw new NotSupportedException();
         }
